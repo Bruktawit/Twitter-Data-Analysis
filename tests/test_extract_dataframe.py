2703e832ad95782ed5bd9310642f69b7b4934007
Bruktawit Teklay <bruktawitteklay23@gmail.com>
2022-04-26 20:05:27 +0300

    update test functions

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join('../..')))
 from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
 
-_, tweet_list = read_json("data/covid19.json")
+_, tweet_list = read_json("data/Economic_Twitter_Data.json")
 
 columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
@@ -74,11 +74,22 @@ class TestTweetDfExtractor(unittest.TestCase):
     def test_find_retweet_count(self):
         self.assertEqual(self.df.find_retweet_count(), [612, 92, 1, 899, 20])
 
-    # def test_find_hashtags(self):
-    #     self.assertEqual(self.df.find_hashtags(), )
+    def test_find_hashtags(self):
+        hashtags = [[], [], [], [], []]
+        self.assertEqual(self.df.find_hashtags(), hashtags)
 
-    # def test_find_mentions(self):
-    #     self.assertEqual(self.df.find_mentions(), )
+    def test_find_mentions(self):
+        user_mentions = [[{"screen_name": "nikitheblogger", "name": "Neverforgetniki", "id": 809188392089092097,
+                           "id_str": "809188392089092097", "indices": [3, 18]}],
+                         [],
+                         [{"screen_name": "sagt_mit", "name": "Sie sagt es mit Bildern", "id": 1511959918777184256,
+                           "id_str": "1511959918777184256", "indices": [3, 12]}],
+                         [],
+                         [{"screen_name": "Kryptonoun", "name": "Kryptoguru", "id": 951051508321345536,
+                           "id_str": "951051508321345536", "indices": [3, 14]},
+                          {"screen_name": "WRi007", "name": "Wolfgang Berger", "id": 1214543251283357696,
+                           "id_str": "1214543251283357696", "indices": [16, 23]}]]
+        self.assertEqual(self.df.find_mentions(), user_mentions)
 
     def test_find_location(self):
         self.assertEqual(self.df.find_location(), ['Mass', 'Edinburgh, Scotland', None, None, 'United Kingdom'])
